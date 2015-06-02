ead6aec8757d1eff1f47ffe2c05b68ff7fdcfaee
Daisy Molving <daisymolving@gmail.com>
2015-06-02 19:43:48 +0100

    created edit methods with links on index page

--- app/controllers/brassicas_controller.rb
+++ app/controllers/brassicas_controller.rb
@@ -10,6 +10,29 @@ class BrassicasController < ApplicationController
     @brassica = Brassica.find(params[:id])
   end
 
+  # GET
+  def new
+    @brassica = Brassica.new
+  end
+
+   # POST 
+  def create
+    Brassica.create(brassica_params)
+    redirect_to(brassicas_path)
+  end
+
+   # GET
+  def edit
+    @brassica = Brassica.find(params[:id])
+  end
+
+  # PUT
+  def update
+    @brassica = Brassica.find(params[:id])
+    @brassica.update(brassica_params)
+    redirect_to(brassicas_path)
+  end
+
    # DELETE
   def destroy
     brassica = Brassica.find(params[:id])
